dd2846f7163e95e9cc5ca3e2b519fcb09deb9fcb
classicmeone <olamitideooo@gmail.com>
2020-01-16 12:22:02 +0100

    Static URL

--- Faces/urls.py
+++ Faces/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     
     path("", views.index, name="home"),
     path("formpage", views.loadform, name="formpage"),
-    path("search", views.search, name="search"),  
+    path("search", views.search, name="search"), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Register/settings.py
+++ Register/settings.py
@@ -122,6 +122,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT= os.path.join(BASE_DIR,'static-files')
+
+STATICFILES_DIRS = [
+    'D:/home/site/wwwroot/static',
+    os.path.join(BASE_DIR,'static'),
+]
+
 MEDIA_URL = "/media/"
 
 #OUT = os.path.join(MEDIA_URL, "source/output")
